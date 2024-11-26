62c39d162161dfaef2f1b81ca02ae1f5af5a3a0b
ArghenXpro <166065495+ArghenXpro@users.noreply.github.com>
2024-11-26 13:26:17 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_i5wzxGA8L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6n7gq2edjd4cnaan9vhof30k1k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_SkfUPS8vj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7li95vivdlf5jkjfne4c4ob7e2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
